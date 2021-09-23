c0cbc55a7af1e37ea6d4ac8c4efdd79279d2d577
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-23 17:32:28 -0400

    25 shell

---
+++ excersice24/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Z
+*src/test/java/baseline/Solution24Test.java,b\8\b8c7d6a717f0fad091fd8f900ccb0ace26334cf5
---
+++ excersice25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+
+public class Solution25
+{
+    //
+
+    private static Scanner input = new Scanner(System.in);
+
+    private int passwordValidator(String pass)
+    {
+        //break up pass into array of char
+        //go through each char and determine value
+        //if char is a num flag value is 1 if letter flag 2 if flag is 2 and length > 8 flag is 3 etc
+        //return value 0-4 depending on pass strength
+    }
+
+    private void printResult(int result)
+    {
+        //takes value of passwordValidator and prints appropriate message
+    }
+
+    public static void main(String[] args)
+    {
+        //prompt user for a password
+        //pass off password to passValidator
+    }
+}
