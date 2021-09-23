b8ed97f8fd8fe8f59487794caae70519d1084520
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-22 21:50:11 -0400

    24 shell

---
+++ excersice24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,30 @@
+package baseline;
+import java.util.Scanner;
+import java.util.Arrays;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+public class Solution24
+{
+    // need to take in strings and break them apart into arrays of chars so we can access each individual letter
+    // check if strings are anagrams
+
+    private static Scanner input = new Scanner(System.in);
+
+    private boolean anagramCheck(String str1, String str2)
+    {
+        //break string into char array
+        //use char array to compare letters in each string
+        //return true if anagram else return false
+    }
+
+    public static void main(String[] args)
+    {
+        //prompt user for strings to check
+        //pass off to anagram check
+        //if returns true print true statement else print false statement
+    }
+}
