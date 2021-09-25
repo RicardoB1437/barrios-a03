ffbf2699beb251989f121f952849cac436c0d2dc
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-25 17:36:49 -0400

    30 shell

---
+++ excersice30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution30
+{
+    //Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+    //Use a nested loop to complete this program.
+    //Align each column within the table without using the tab character.
+
+    //create Table class and put functions inside it
+
+    private void makeRow(int baseNum)
+    {
+        //String space = "     "; (5 spaces)
+        //for loop through 12 times and multiply baseNum by i
+    }
+
+    public static void main(String[] args)
+    {
+        //for loop through 12 times and call makeRow(i)
+    }
+}
