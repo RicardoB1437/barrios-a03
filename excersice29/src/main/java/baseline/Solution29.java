3ab8c73ef8f7ba31a762a4e9b7d6ab8b62d3cc79
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-25 02:35:21 -0400

    29 shell

---
+++ excersice29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution29
+{
+    //Don’t allow the user to enter 0.
+    //Don’t allow non-numeric values.
+    //Use a loop to trap bad input, so you can ensure that the user enters valid values.
+    //Not really a need for another class
+
+    private int takeInput()
+    {
+        //use throw catch for exceptions
+    }
+
+    public static void main(String[] args)
+    {
+        //Solution28 sol = new Solution28();
+        //int years = 72 / takeInput();
+        //System.out.printf("It will take %d years to double your initial investment.");
+    }
+}
