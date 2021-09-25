1dbaa2346ea41619d196381676f96707ddfbfe36
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-25 01:30:58 -0400

    28 shell

---
+++ excersice28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+
+public class Solution28
+{
+    //Write a program that prompts the user for five numbers and computes the total of the numbers.
+    //The prompting must use repetition, such as a counted loop, not three separate prompts.
+    //This code is extremely simple and using separate classes would just be over the top
+
+    Scanner input = new Scanner(System.in);
+
+    private int addNums()
+    {
+        //loop through 5 times and ask for a number
+        //int total = 0;
+        //int total += input.nextInt()
+    }
+
+    public static void main(String[] args)
+    {
+        //Solution28 sol = new Solution28();
+        //int total = sol.addNums()
+        //System.out.printf("The total is %d.", total);
+    }
+}
