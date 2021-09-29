4dd3282069cbf6e7e0e70ad2136730cac7f69f5a
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-29 19:36:44 -0400

    33 shell

---
+++ excersice33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.*;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+
+public class Solution33
+{
+    //Create a Magic 8 Ball game that prompts for a question
+    //and then displays either "Yes," "No," "Maybe," or "Ask again later."
+    //create class with a function that holds arrayList
+
+    Scanner input = new Scanner(System.in);
+
+    public String magicAnswer(int randNum)
+    {
+        //List<Integer> list1 = new ArrayList<String>();
+        //return list1.get(random number);
+    }
+
+    public static void main(String[] args)
+    {
+        //prompt user for question
+        //create magic num from 1-4
+        //String answer = magicAnswer(randNum);
+        //print out answer
+    }
+}
