8d447b028a79f97fa58e30ebb9afb8a9b95356a7
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-30 00:53:09 -0400

    35 status

---
+++ excersice35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,58 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.*;
+import java.util.Random;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution35
+{
+    //
+
+    private String takeName()
+    {
+        //System.out.print("Enter a name: ");
+        //return input.nextLine();
+    }
+
+    public List<String> makeList()
+    {
+        //List<String> names = new ArrayList<String>();
+        //while loop
+        //call takeName
+        //add to list until takeName returns empty string
+        //return list
+    }
+
+    public String pickWinner(List<String> names, int length)
+    {
+        //Random rand = new Random();
+        //int winNum = rand.nextInt(length);
+        //return names.get(winNum);
+    }
+
+    public static void main(String[] args)
+    {
+        ////Lottery lot = new Lottery();
+
+        //create list
+        //List<String> names = lot.makeList();
+
+        //get list length
+        //listLength = names.size();
+
+        //get winner
+        //String winner = pickWinner(names, listLength);
+
+        //print winner
+        //System.out.printf("The winner is... %s", winner);
+    }
+}
