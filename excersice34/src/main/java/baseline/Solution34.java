280553891f9709518978e0fb479a036a3e70b3e4
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-29 21:00:02 -0400

    34 shell

---
+++ excersice34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,61 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.*;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution34
+{
+    //
+
+    public String takeName()
+    {
+        //System.out.print("%nEnter an employee name to remove: ");
+        //return input.nextLine();
+    }
+
+    public List<String> makeList()
+    {
+        //List<String> employees = new ArrayList<String>();
+        //add all employees to list
+        //return list
+    }
+
+    public void printList(List<String> employees)
+    {
+        //System.out.printf("There are %d employees:%n", employees.size());
+        //loop through 5 times and print names of employees in list
+    }
+
+    //return string removed for testing purposes
+    public String removeName(List<String> employees, String input)
+    {
+        //print list
+        //loop through whole list
+        //look for input in the list
+        //if found remove it
+        //print list
+    }
+
+    public static void main(String[] args)
+    {
+        //EmployeeList emp = new EmployeeList();
+
+        //create list
+        //List<String> employees = emp.makeList();
+
+        //ask user for name to look for
+        //String searchName = emp.takeName();
+
+        //remove employee
+        //emp.removeName(employees, searchName);
+    }
+}
