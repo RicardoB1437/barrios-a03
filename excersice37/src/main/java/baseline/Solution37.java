6d5771df7572764ea02ef8ba7dd40c8645b51c03
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-09-30 22:07:03 -0400

    37 shell

---
+++ excersice37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,76 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.*;
+import java.util.Random;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution37
+{
+    //
+
+    public int takePassLength()
+    {
+        //System.out.println("What's the minimum length? ");
+        //return input.nextInt();
+    }
+
+    public int takeNumSpecial()
+    {
+        //System.out.println("How many special characters? ");
+        //return input.nextInt();
+    }
+
+    public int takeNumNumbers()
+    {
+        //System.out.println("How many numbers? ");
+        //return input.nextInt();
+    }
+
+    public List<String> makeCharList();
+    {
+        //add in letters a-z to list
+        //return list
+    }
+
+    public List<String> makeSpecialList();
+    {
+        //add in special characters to list
+        //return list
+    }
+
+    public String makePassword(List<String> chars, List<String> spec, int passLength, int numNumbers, int numSpecial)
+    {
+        //Random rand = new Random();
+        //for loop through passLength
+        //generate rand num from 1-3
+        //if 1 add letter to pass //letter++
+        //if 2 add special to pass //special++
+        //if 3 add rand num (0-9) to pass //num++
+        //go until numbers specified in parameter
+    }
+
+    public static void main(String[] args)
+    {
+        //List<String> chars = pass.makeList();
+        //List<String> special = pass.makeList();
+
+        //prompt for length, num chars, num special
+        //int passLength = pass.takePassLength();
+        //int numNumbers = pass.takeNumNumbers();
+        //int numSpecial = pass.takeNumSpecial();
+
+        //make password
+        //String password = pass.makePassword(chars, special, passLength, numNumbers, numSpecial)
+        //print password
+        //System.out.printf("Your password is %s", password);
+    }
+}
