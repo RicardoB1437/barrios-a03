ab08ec19a1328a03f923f6d9875f8b184c9bd382
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-10-01 16:23:29 -0400

    38 shell

---
+++ excersice38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,60 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.Scanner;
+import java.util.*;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution38
+{
+    //
+
+    public String takeInput()
+    {
+        //System.out.print("Enter a list of numbers, separated by spaces: ");
+        //return input.nextLine();
+    }
+
+    public List<Integer> filterSpaces(String input)
+    {
+        //take the input string and go to each index
+        //if cant be parsed into int then skip index
+        //parse int and add int to list
+        //return list
+    }
+
+    public List<Integer> filterEvenNumbers(List<Integer> nums)
+    {
+        //make evenList
+        //loop through length of nums list
+        //if(nums.get(i) %2 == 0) evenList.add(nums.get(i))
+        //return evenList
+    }
+
+    public void printList(List<Integer> nums)
+    {
+        //loop through length of list
+        //print out nums
+    }
+
+    public static void main(String[] args)
+    {
+        //NumList numbers = new NumList();
+        //prompt user for list of numbers
+        //String input = numbers.takeInput();
+        //take input and put into list of nums
+        //List<Integer> nums = numbers.filterSpaces(input);
+        //take list of nums and filter even nums
+        //List<Integer> evenNums = numbers.filterEvenNumbers(nums);
+        //print list
+        //numbers.printList(evenNums);
+
+    }
+}
